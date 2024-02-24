fb386b95d44bcc91d795ee9668f7cf9cd8686555
Michal Uzdowski <uzdowski.design@gmail.com>
2024-02-24 23:01:49 +0100

    create user model

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import router from './router';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+  console.log('Server running on http://localhost:8080/');
+});
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on('error', (error: Error) => console.log(error));
+
+app.use('/', router());
